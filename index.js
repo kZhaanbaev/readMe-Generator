3f1cac24af3aa8b083c7c36d0c23c2f3d0a511e7
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-04-18 09:16:07 -0400

    added inquirer function that promts questions and captures answer object

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
     'What is the project title?',
-    'Add a main screenshot of the project?',
-    'What will be a description?',
+    'Add a main screenshot of the project? (Screenshot is located in /utils/images/screenshot.png, save your image with same name in the same location)',
+    'What will be the description?',
     'Installation instructions?',
     'How to use the application?',
     'Information about contributors?',
@@ -15,11 +16,84 @@ const questions = [
     "email address?"
 ];
 
+const fileContent = (answers, licenseBadge) =>
+`# ${answers.title}
+    ${licenseBadge}
+    <br /><br />
+    ![image of application](./utils/image/screenshot.png)    
+`
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.write(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Successfully added to File'));
+}
+
+function promptQuestions(){
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: questions[0]
+            },
+            {
+                type: 'confirm',
+                name: 'addScreenshot',
+                message: questions[1]
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: questions[2]
+            },
+            {
+                type: 'input',
+                name: 'installation',
+                message: questions[3]
+            },
+            {
+                type: 'input',
+                name: 'usage',
+                message: questions[4]
+            },
+            {
+                type: 'input',
+                name: 'contributors',
+                message: questions[5]
+            },
+            {
+                type: 'input',
+                name: 'tests',
+                message: questions[6]
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: questions[7],
+                choices: ['Apache 2.0 License', 'The MIT License', 'Open Database License (ODbL)', 'WTFPL']
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: questions[8]
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: questions[9]
+            },
+        ])
+        .then(answers => {
+            
+        })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptQuestions();
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  switch (license) {
+    case 'Apache 2.0 License': return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    case 'The MIT License': return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    case 'Open Database License (ODbL)': return '[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)]';
+    case 'WTFPL': return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)';
+    default: return '';
+  }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  switch (license) {
+    case 'Apache 2.0 License': return 'https://opensource.org/licenses/Apache-2.0';
+    case 'The MIT License': return 'https://opensource.org/licenses/MIT';
+    case 'Open Database License (ODbL)': return 'https://opendatacommons.org/licenses/odbl/';
+    case 'WTFPL': return 'http://www.wtfpl.net/about/';
+    default: return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
