0f5e7422fe95a3c5c4036deceff307ffbc666603
Kuba Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-04-17 22:40:33 -0400

    added utils folder along with index js and html files

---
+++ index.js
@@ -0,0 +1,25 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the project title?',
+    'Add a main screenshot of the project?',
+    'What will be a description?',
+    'Installation instructions?',
+    'How to use the application?',
+    'Information about contributors?',
+    'Any Tests included and how to run them?',
+    'Choose one of the following licenses:',
+    'Github username?',
+    "email address?"
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
